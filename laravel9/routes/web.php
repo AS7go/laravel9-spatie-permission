2b2e7e0865e6b7b6e21868844d4c154153df80a1
Aleksandr <sh17aleksandr@gmail.com>
2024-05-07 19:07:58 +0300

    role checked

--- laravel9/routes/web.php
+++ laravel9/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
@@ -28,13 +29,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// });
 
-Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
-Route::get('add-post', [PostController::class, 'create'])->name('add-post');
-Route::post('store-post', [PostController::class, 'store'])->name('store-post');
-Route::get('edit-post/{id}', [PostController::class, 'edit'])->name('edit-post');
-Route::put('update-post/{id}', [PostController::class, 'update'])->name('update-post');
-Route::delete('delete-post/{id}', [PostController::class, 'delete'])->name('delete-post');
+// Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
+    Route::get('add-post', [PostController::class, 'create'])->name('add-post');
+    Route::post('store-post', [PostController::class, 'store'])->name('store-post');
+    Route::get('edit-post/{id}', [PostController::class, 'edit'])->name('edit-post');
+    Route::put('update-post/{id}', [PostController::class, 'update'])->name('update-post');
+    Route::delete('delete-post/{id}', [PostController::class, 'delete'])->name('delete-post');
+    
+    Route::resource('roles', RoleController::class);
+});
 
 require __DIR__ . '/auth.php';
